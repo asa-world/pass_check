85b67962cc1b18837429906bd0d030cf6f956b00
asa-world <1923793974@qq.com>
2022-10-02 23:33:06 +0800

    mender

--- source/action.js
+++ source/action.js
@@ -1,4 +1,4 @@
 // Get the snackbar DIV
 var x = document.getElementById("snackbar")
 // After 3 seconds, remove the show class from DIV
-setTimeout(function(){ x.className =x.className.replace("show", ""); },1000);
+setTimeout(function(){ x.className =x.className.replace("show", ""); },1800);
