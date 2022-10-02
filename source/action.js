850554fd9573fb311887695d1d909f4bddc5e840
asa-world <1923793974@qq.com>
2022-10-02 21:48:55 +0800

    add snackbar

---
+++ source/action.js
@@ -0,0 +1,4 @@
+// Get the snackbar DIV
+var x = document.getElementById("snackbar")
+// After 3 seconds, remove the show class from DIV
+setTimeout(function(){ x.className =x.className.replace("show", ""); },1000);
